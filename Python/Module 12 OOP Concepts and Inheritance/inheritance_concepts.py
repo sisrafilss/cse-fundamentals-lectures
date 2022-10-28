1047fcbc921ab7f6fb07e836433db8d0e6904b08
Israfil Hossen <sisrafilss@gmail.com>
2022-10-28 10:37:37 +0600

    till module 12 completed

---
+++ Python/Module 12 OOP Concepts and Inheritance/inheritance_concepts.py
@@ -0,0 +1,14 @@
+# parent class
+# base class
+class BaseClass:
+    pass
+
+# derived class
+# child class
+class DerivedClass(BaseClass):
+    pass
+
+# 1. Single inheritance (parent --> child)
+# 2. Multi Level Inheritance (grandpa --> parent --> child)
+# 3. Multiple Inheritance (father, monther: child --> (father, mother))
+# 4. Hierarchical Inheritance (father --> you, father --> brother, father --> sister)
